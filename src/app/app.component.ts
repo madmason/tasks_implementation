274be2ebedae21d8f3c8f9413391313f59c6d2a5
seb <madmason@users.noreply.github.com>
2022-05-01 22:40:51 +0200

    use input binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tasks_implementation';
+  book = {
+    title: "Thief of Time",
+    author: "Terry Pratchett",
+    description: "A sweeper saves time on the Discworld"
+  };
 }
--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book-card',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class BookCardComponent implements OnInit {
 
   constructor() { }
+
+  @Input() content: any;
 
   ngOnInit(): void {
   }
